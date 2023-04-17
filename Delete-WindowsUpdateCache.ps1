fb6fdbebd3edb3513955dd9de78837d93155b559
Don <deuler93@gmail.com>
2023-04-16 21:24:45 -0500

    Upload Delete-WindowsUpdateCache.ps1

---
+++ Delete-WindowsUpdateCache.ps1
@@ -0,0 +1,5 @@
+﻿
+#Requires -RunAsAdministrator
+Start-Process NET -ArgumentList "STOP wuauserv"
+$path = 'C:\Windows\SoftwareDistribution\Download'
+Get-ChildItem -Path $path -File | Remove-Item -Force
